674b9784ee082f9d37de5660aaf69a664420a670
EricTTFO <34400777+EricTTFO@users.noreply.github.com>
2019-05-11 19:09:19 +0800

    S

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,75 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <h1>{{ count }}</h1>
+      <p>{{ completedTodos }}</p>
+      <h2>{{ completedTodosCount }}</h2>
+    <h3> {{ getTodosById(2) }} </h3>-->
+
+    <!-- mutations -->
+    <span>{{ count }}</span>
+
+    <button @click="decrement({ amout: 1 })">-</button>
+    
+    <button @click="increment">+</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { mapState, mapGetters } from "vuex";
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  components: {},
+  computed: mapGetters([
+    "count",
+    "completedTodos",
+    "completedTodosCount",
+    "getTodosById"
+  ]),
+  methods: {
+    increment(){
+      // console.log(1);
+      //触发mutation里面的方法
+      this.$store.commit('incrementCount');
+    },
+    decrement(n){
+      // console.log(0);
+      this.$store.commit('decrementCount', n)
+    }
   }
-}
+  // computed: {
+  //   count(){
+  //     return this.$store.getters.count;
+  //   },
+  //   completedTodos(){
+  //     return this.$store.getters.completedTodos;
+  //   },
+  //   completedTodosCount(){
+  //     return this.$store.getters.completedTodosCount;
+  //   },
+  //   getTodosById(){
+  //     return this.$store.getters.getTodosById;
+  //   }
+  // }
+  // state & mapState
+  // computed:{
+  //   count(){
+  //    return this.$store.state.count;
+  //   },
+  //   todos(){
+  //     return this.$store.state.todos;
+  //   }
+  // }
+  // computed: mapState({
+  //   count: state => state.count,
+  //   todos: state => state.todos
+  // })
+  // computed: mapState(['count','todos']) //mapState方法省略获取方式 ，并不常用 无法操作数据
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
